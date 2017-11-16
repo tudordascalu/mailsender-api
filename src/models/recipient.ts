1732132579b76b2182a831269eec8631faffe5a5
Tudor Dascalu <tudordascalu1@gmail.com>
2017-11-16 19:15:20 +0100

    Added new fields to recipient model

--- src/models/recipient.ts
+++ src/models/recipient.ts
@@ -1,10 +1,14 @@
 export class RecipientList
 {
-    static keys = ['id', 'recipients', 'owners'];
+    static keys = ['id', 'recipients', 'owners', 'listName', 'editDate', 'numberOfEmails', 'status'];
 
     id?: string;
     recipients?: string[];
     owners?: string[];
+    listName?: string;
+    editDate?: string;
+    numberOfEmails?: number;
+    status?: string;
 
     get dbData()
     {
@@ -26,6 +30,7 @@ export class RecipientList
     public static fromRequest(data: any): RecipientList
     {
         const site = new RecipientList(data);
+        
         return site;
     }
 
@@ -36,21 +41,24 @@ export class RecipientList
         return site;
     }
 
-    public updateRecipients(data: { deleteRecipients: string[], addRecipients: string[] })
+    public updateRecipients(recipients: string[])
     {
-        for (let i = 0; i < data.deleteRecipients.length; i++)
-        {
-            const recipient = data.deleteRecipients[i];
-            const index = this.recipients.indexOf(recipient);
-            if (index !== -1) { this.recipients.splice(index, 1); }
-        }
+    //     for (let i = 0; i < data.deleteRecipients.length; i++)
+    //     {
+    //         const recipient = data.deleteRecipients[i];
+    //         const index = this.recipients.indexOf(recipient);
+    //         if (index !== -1) { this.recipients.splice(index, 1); }
+    //     }
 
-        for (let i = 0; i < data.addRecipients.length; i++)
-        {
-            const recipient = data.addRecipients[i];
-            const index = this.recipients.indexOf(recipient);
-            if (index === -1) { this.recipients.push(recipient); }
-        }
+    //     for (let i = 0; i < data.addRecipients.length; i++)
+    //     {
+    //         const recipient = data.addRecipients[i];
+    //         const index = this.recipients.indexOf(recipient);
+    //         if (index === -1) { this.recipients.push(recipient); }
+    //     }
+        this.recipients = recipients;
+        this.numberOfEmails = this.recipients.length;
+        this.editDate = RecipientList.getEditDate();
     }
 
     public get responseData()
@@ -59,7 +67,36 @@ export class RecipientList
         {
             id: this.id,
             recipients: this.recipients,
+            listName: this.listName,
+            editDate: this.editDate,
+            numberOfEmails: this.numberOfEmails,
+            status: this.status
         };
         return parameters;
+    }
+
+    public static getEditDate(){
+        var currentDate = new Date();
+        
+        var date = currentDate.getDate();
+        var month = currentDate.getMonth(); //Be careful! January is 0 not 1
+        var year = currentDate.getFullYear();
+        
+        var minutes = currentDate.getMinutes();
+        var hours = currentDate.getHours();
+        
+        var testHours = (hours+24-2)%24; 
+        var mid='AM';
+        if(hours==0){ //At 00 hours we need to show 12 am
+            hours=12;
+        }
+        else if(hours>12)
+        {
+            hours=hours%12;
+            mid='PM';
+        }
+        
+        var dateString = date + "-" +(month + 1) + "-" + year + " " + hours + "." + minutes + mid;
+        return dateString;
     }
 }
